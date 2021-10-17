16168b8e0cf57b406abe6e96cd562c971ed26558
Tpopescu33 <85653313+Tpopescu33@users.noreply.github.com>
2021-10-16 20:26:52 -0500

    test

--- FinalProject_Popescu_Tudor/ViewController.swift
+++ FinalProject_Popescu_Tudor/ViewController.swift
@@ -7,13 +7,78 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+struct Sections: Codable {
+    let title: String
+    let options: [String]
+    let isOpen: Bool
+    
+    init() {
+        self.title = ""
+        self.options = [""]
+        self.isOpen = false
     }
-
-
+    
+    
 }
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+   
+    
+    var sections = [Sections]()
+    
+
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        let section = sections[section]
+        
+        if section.isOpen {
+            return section.options.count + 1
+        } else {
+            return 1
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if indexPath.row == 0 {
+            let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "incomeCell")
+            cell.textLabel?.text = sections[indexPath.section].title
+        
+        
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label1: UILabel!
+    
+    
+    @IBOutlet weak var IncomeTable: UITableView!
+    
+    @IBOutlet weak var AddExpenseButton: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+       
+        
+    }
+    
+    
+
+}
