1e8bfc75485cffde731d2e0d9eea35b4ba6db1b8
Tpopescu33 <85653313+Tpopescu33@users.noreply.github.com>
2021-10-17 09:40:23 -0500

    cells expand on main view controller

--- FinalProject_Popescu_Tudor/ViewController.swift
+++ FinalProject_Popescu_Tudor/ViewController.swift
@@ -7,57 +7,13 @@
 
 import UIKit
 
-struct Sections: Codable {
-    let title: String
-    let options: [String]
-    let isOpen: Bool
-    
-    init() {
-        self.title = ""
-        self.options = [""]
-        self.isOpen = false
-    }
-    
-    
-}
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+class ViewController: UIViewController {
     
    
     
-    var sections = [Sections]()
-    
 
-    
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        let section = sections[section]
-        
-        if section.isOpen {
-            return section.options.count + 1
-        } else {
-            return 1
-        }
-        
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        if indexPath.row == 0 {
-            let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "incomeCell")
-            cell.textLabel?.text = sections[indexPath.section].title
-        
-        
-            return cell
-        }
-        return UITableViewCell()
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return sections.count
-    }
-    
 
     @IBOutlet weak var label2: UILabel!
     @IBOutlet weak var label1: UILabel!
